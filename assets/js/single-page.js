5aa2e116644aa887427c33809e7448517ffdbf7e
JRM <johnrhmartin@gmail.com>
2022-02-15 21:20:34 -0500

    updated the buttons to be side by side and lowered the opacity

--- assets/js/single-page.js
+++ assets/js/single-page.js
@@ -23,36 +23,40 @@ galleryArray = [];
 console.log(galleryImg);
 
 var gallery = function() {
-        for (var i = 0; i < galleryImg.length; i++) {
-                if (galleryImg.length > 6) {
-                        console.log("!!!");
-                        galleryImg.shift();
+        if (galleryImg) {
+                for (var i = 0; i < galleryImg.length; i++) {
+                        if (galleryImg.length > 6) {
+                                console.log("!!!");
+                                galleryImg.shift();
+                        }
                 }
+                theIMG_1.innerHTML = galleryImg[0].picture;
+                theTitle_1.innerHTML = galleryImg[0].title;
+                theDescription_1.innerHTML = galleryImg[0].description;
+
+                theIMG_2.innerHTML = galleryImg[1].picture;
+                theTitle_2.innerHTML = galleryImg[1].title;
+                theDescription_2.innerHTML = galleryImg[1].description;
+
+                theIMG_3.innerHTML = galleryImg[2].picture;
+                theTitle_3.innerHTML = galleryImg[2].title;
+                theDescription_3.innerHTML = galleryImg[2].description;
+
+                theIMG_4.innerHTML = galleryImg[3].picture;
+                theTitle_4.innerHTML = galleryImg[3].title;
+                theDescription_4.innerHTML = galleryImg[3].description;
+
+                theIMG_5.innerHTML = galleryImg[4].picture;
+                theTitle_5.innerHTML = galleryImg[4].title;
+                theDescription_5.innerHTML = galleryImg[4].description;
+
+                theIMG_6.innerHTML = galleryImg[5].picture;
+                theTitle_6.innerHTML = galleryImg[5].title;
+                theDescription_6.innerHTML = galleryImg[5].description;
         }
         
-        theIMG_1.innerHTML = galleryImg[0].picture;
-        theTitle_1.innerHTML = galleryImg[0].title;
-        theDescription_1.innerHTML = galleryImg[0].description;
-
-        theIMG_2.innerHTML = galleryImg[1].picture;
-        theTitle_2.innerHTML = galleryImg[1].title;
-        theDescription_2.innerHTML = galleryImg[1].description;
-
-        theIMG_3.innerHTML = galleryImg[2].picture;
-        theTitle_3.innerHTML = galleryImg[2].title;
-        theDescription_3.innerHTML = galleryImg[2].description;
-
-        theIMG_4.innerHTML = galleryImg[3].picture;
-        theTitle_4.innerHTML = galleryImg[3].title;
-        theDescription_4.innerHTML = galleryImg[3].description;
-
-        theIMG_5.innerHTML = galleryImg[4].picture;
-        theTitle_5.innerHTML = galleryImg[4].title;
-        theDescription_5.innerHTML = galleryImg[4].description;
-
-        theIMG_6.innerHTML = galleryImg[5].picture;
-        theTitle_6.innerHTML = galleryImg[5].title;
-        theDescription_6.innerHTML = galleryImg[5].description;
+        
+        
 
         
 };
